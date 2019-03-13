b1d4ca07c355700d14fdd2fd80d9b141ed397c65
Lehel Kovach <drdorianwhite@gmail.com>
2019-03-13 20:38:23 +0000

    new componenents for report

---
+++ components/report/ReportSummary.js
@@ -0,0 +1,15 @@
+import moduleName from 'module'
+
+import React, { Component } from 'react'
+import { Text, View } from 'react-native'
+
+export default class ReportSummary extends Component {
+  render() {
+    return (
+      <View>
+        <Text> textInComponent </Text>
+      </View>
+    )
+  }
+}
+    
